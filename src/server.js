60f51fec10e15e8b57a30bcb15866460a8ea2fe0
Mathias Tøgersen Christensen <math7213@student.eadania.dk>
2019-08-15 17:43:00 +0200

    Fixed module issue

--- src/server.js
+++ src/server.js
@@ -1,10 +1,14 @@
-import express from 'express';
+const express = require('express');
 
 const app = express();
 
 app.listen(3000, () =>
   console.log('Example app listening on port 3000!'),
 );
+
+app.get('/', function(req, res) {
+    res.send('This is the root route')
+})
 
 app.get('/data', function(req, res) {
     res.send('Here you can GET data')
